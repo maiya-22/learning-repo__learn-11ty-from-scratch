1d7863c67f5151239ff3afecf04cb182a5ae4f02
maiya <maiya@MacBook-Air.local>
2023-03-27 19:43:02 -0400

    lesson 8 creating a collection in progress.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
 module.exports = config => {
     // Set directories to pass through to the dist folder
     config.addPassthroughCopy('./src/images/');
+    // Returns work items, sorted by display order
+    config.addCollection('work', collection => {
+      return collection
+        .getFilteredByGlob('./src/work/*.md')
+        .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1));
+    });
     return {
         markdownTemplateEngine: 'njk',
         dataTemplateEngine: 'njk',
